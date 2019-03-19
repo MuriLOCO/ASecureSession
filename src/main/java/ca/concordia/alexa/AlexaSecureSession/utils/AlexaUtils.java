6149cc7528b8fceaf459db9576cd31f0ce0469f1
Jose Mendes <jose.murilo.mendes@gmail.com>
2019-03-18 23:32:16 -0400

    Some improvement on generating the challenge.

--- src/main/java/ca/concordia/alexa/AlexaSecureSession/utils/AlexaUtils.java
+++ src/main/java/ca/concordia/alexa/AlexaSecureSession/utils/AlexaUtils.java
@@ -2,7 +2,7 @@ package ca.concordia.alexa.AlexaSecureSession.utils;
 
 public class AlexaUtils {
 
-  public final static String ALEXA_API_KEY = "amzn1.ask.skill.34c79971-ef54-414a-885b-1f5d25f3f2c0";
+  public final static String ALEXA_API_KEY = "amzn1.ask.skill.6b183ddc-d527-4e83-a8d5-656083506666";
   
   public final static String URL_SECURE_SESSION = "asecuresession.ddns.net:8080/generate?keySize=1024";
   
